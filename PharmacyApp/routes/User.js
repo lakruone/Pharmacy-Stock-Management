ccae0acd076320e50f5a6cc214a0aa8684864af2
lakruone <lakruone@gmail.com>
2020-05-19 15:36:16 +0530

    bug fix in activate and home templates

--- PharmacyApp/routes/User.js
+++ PharmacyApp/routes/User.js
@@ -50,7 +50,6 @@ router.post('/activate_account', (req, res) => {
   const host = req.get('host');
   const protocol = req.protocol;
 
-
 	console.log(username);
 
 	userData = {
@@ -86,7 +85,7 @@ router.post('/activate_account', (req, res) => {
           return res.json({message :2});
 
         }else {
-          res.json({message :0});
+         res.json({message :0});  
 
           ///////////////generate token//////////////
           jwt.sign({userData},'LakruSecret',{ expiresIn: '24h' }, (err,token)=>{
@@ -289,130 +288,6 @@ router.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
-//user registration
-// router.get('/register', (req, res) => {
-//
-// 	res.render('register', {
-// 		title : 'Register CHRN_Sales',
-// 		style : 'register.css',
-// 		// script : 'preloader.js'
-// 	});
-// });
-
-// router.post('/register', (req, res) => {
-// 	// res.render('register', {
-// 	// 	title : 'Register CHRN_Sales',
-// 	// 	style : 'register.css',
-// 	// 	// script : 'preloader.js'
-// 	// });
-//
-// 	let errors = [];
-//
-// 	if(!req.body.fname){
-// 		errors.push({text: 'Please enter the Firstname'});
-// 	}
-//
-// 	if(!req.body.password1){
-// 		errors.push({text: 'Please enter the Password'});
-// 	}else if(req.body.password1 != req.body.password2){
-// 		errors.push({text: 'Password do not match'});
-// 	}
-//
-// 	if(!req.body.email){
-// 		errors.push({text: 'Please enter the Email'});
-// 	}
-//
-// 	if (errors.length > 0) {
-// 		res.render('register', {
-// 			title : 'Register CHRN_Sales',
-// 			style : 'register.css',
-// 			errors : errors,
-// 			fname : req.body.fname,
-// 			password1 : req.body.password1,
-// 			email : req.body.email
-// 		});
-// 	}else{
-// 		const newUser = {
-// 			firstName: req.body.fname,
-// 			lastName: req.body.lname,
-// 			password: req.body.password1,
-// 			gender: req.body.gender,
-// 			email: req.body.email,
-// 			telephone: req.body.telephone,
-// 			pharmacy: req.body.pharmacy,
-// 			passHint: req.body.passHint
-// 		}
-//
-// 		new User(newUser).save().then(user =>{
-// 			res.redirect('/login');
-// 		})
-// 	}
-//
-// 	console.log(req.body);
-// 	console.log(errors);
-//
-// });
-//
-//
-//
-//
-// router.get('/login', (req, res) => {
-// 	res.render('login', {
-// 		title : 'CHRN Login',
-// 		style : 'login.css',
-// 		// script : 'main.js'
-// 	});
-// });
-
-// router.post('/login', (req, res) => {
-// 	const email = req.body.email;
-// 	const password = req.body.password;
-//
-// 	User.find({email:email})
-//     .then(user => {
-//     	if (!user) {
-//     		res.render('login', {
-//     			title : 'CHRN Login',
-// 				style : 'login.css',
-// 				error : 'Invalid User'
-//     		});
-//     	}else{
-// 					res.redirect('/dashboard');
-//     		// res.render('dashboard', {
-//     		// 	title : 'CHRN Login',
-// 				// style : 'dashboard.css',
-// 				// // success : 'success'
-//     		// });
-//     	}
-//     });
-//
-// 	// res.render('login', {
-// 	// 	title : 'CHRN Login',
-// 	// 	style : 'login.css',
-// 	// 	// script : 'main.js'
-// 	// });
-// });
-
-
-
-// router.get('/dashboard', (req, res) => {
-// 	req.flash('success_msg','you are in the Dashboard');
-// 	res.render('dashboard', {
-// 		title : 'CHRN Dashboard',
-// 		style : 'dashboard.css',
-// 		script : 'dashboard.js'
-// 	});
-// });
-//
-//
-// router.get('/addNewproduct', (req, res) => {
-// 	res.send("helloo");
-// 	// res.render('dashboard', {
-// 	// 	title : 'CHRN Dashboard',
-// 	// 	// style : 'login.css',
-// 	// 	script : 'dashboard.js'
-// 	// });
-// });
 
 
 module.exports = router;
